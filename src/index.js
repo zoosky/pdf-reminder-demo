b29f247f81d78cf20864a92506edc484f230a7b9
Andreas <andreas@Andreass-MacBook-Pro.local>
2017-09-04 16:06:50 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import $ from "jquery";
+import moment from "moment";
+import simpleReminder from "pdf-reminder-simple";
+
+const pdf = simpleReminder({
+  invertHeader: true,
+  organizationAddress: {
+    name: "Orbin",
+    street: "Strasse 333",
+    postCode: "3474",
+    city: "Stadt"
+  },
+  billingAddress: {
+    name: "Mein Kontakt",
+    attn: "Mr. Melk",
+    street: "Strasse 2",
+    postCode: "78556",
+    city: "Andere Stadt"
+  },
+  date: moment(),
+  customerName: "MyCompany GmbH",
+  reminderName: "1. Mahnung",
+  reminderText: "Sie haben die Rechnung nicht bezahlt!",
+  invoiceNumber: 12,
+  invoiceDate: moment(),
+  invoiceTotal: 430,
+  feeAmount: 10,
+  total: 440,
+  currency: "EUR",
+  note: "Meine Notiz"
+});
+pdf.getDataUrl({}, (data) => {
+  $("iframe").attr("src", data);
+});
